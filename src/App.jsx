3d2dd992e9be58bf20f23abcf8b622141f0c6850
Naomi Ansah <ansahnaomi2014@gmail.com>
2025-12-08 11:37:42 +0000

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Resume from "./components/Resume";
-import Services from "./components/services";
-import Certifications from "./components/Certifications";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/contact";
+// import "./App.css";
+// import Footer from "./components/Footer";
+// import Home from "./components/Home";
+// import Navbar from "./components/Navbar";
+// import About from "./components/About";
+// import Resume from "./components/Resume";
+// import Services from "./components/services";
+// import Certifications from "./components/Certifications";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/contact";
 
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Home />
+//       <About />
+//       <Resume />
+//       <Services />
+//       <Certifications />
+//       <Portfolio />
+//       <Contact />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-      <Resume />
-      <Services />
-      <Certifications />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </>
+    <div style={{ padding: "2rem" }}>
+      <h1>Hello from Naomi test 🚀</h1>
+      <p>If you can see this, React + Vercel are working.</p>
+    </div>
   );
 }
 
